b4c0fa50a95d371e9220a0a8af9d67828b4927c7
Ceikry <ironceikry@gmail.com>
2020-11-17 10:28:21 -0600

    Fixed puppy interface

--- Server/src/main/java/Server/plugin/interaction/inter/PuppyInterfacePlugin.kt
+++ Server/src/main/java/Server/plugin/interaction/inter/PuppyInterfacePlugin.kt
@@ -1,42 +1,54 @@
-package plugin.interaction.inter;
+package plugin.interaction.inter
 
-import core.game.component.Component;
-import core.game.component.ComponentDefinition;
-import core.game.component.ComponentPlugin;
-import core.game.node.entity.player.Player;
-import core.game.node.item.Item;
-import core.plugin.Plugin;
-import core.plugin.InitializablePlugin;
-import core.tools.RandomFunction;
+import core.game.component.Component
+import core.game.component.ComponentDefinition
+import core.game.component.ComponentPlugin
+import core.game.node.entity.player.Player
+import core.game.node.item.Item
+import core.plugin.InitializablePlugin
+import core.plugin.Plugin
+import core.tools.RandomFunction
 
 /**
  * Represents the puppy interface plugin.
- * @author 'Vexia
+ * @author Ceikry
  */
 @InitializablePlugin
-public final class PuppyInterfacePlugin extends ComponentPlugin {
+class PuppyInterfacePlugin : ComponentPlugin() {
+    override fun newInstance(arg: Any?): Plugin<Any>? {
+        ComponentDefinition.put(668, this)
+        return this
+    }
 
-	/**
-	 * Represents the names of the puppys.
-	 */
-	private static final String[] NAMES = new String[] { "labrador", "bulldog", "dalmatian", "greyhound", "terrier", "sheepdog" };
+    override fun handle(player: Player, component: Component, opcode: Int, button: Int, slot: Int, itemId: Int): Boolean {
+        val index = when(button){
+            8 -> 0
+            3 -> 1
+            4 -> 2
+            5 -> 3
+            6 -> 4
+            7 -> 5
+            else -> 0
+        }
+        player.dialogueInterpreter.open(6893, NAMES[index], PUPPIES[index][RandomFunction.random(PUPPIES[index].size)])
+        return true
+    }
 
-	/**
-	 * Represents the puppies.
-	 */
-	private static final Item[][] PUPPIES = new Item[][] { { new Item(12516), new Item(12708), new Item(12710) }, { new Item(12522), new Item(12720), new Item(12722) }, { new Item(12518), new Item(12712), new Item(12714) }, { new Item(12514), new Item(12704), new Item(12706) }, { new Item(12512), new Item(12700), new Item(12702) }, { new Item(12520), new Item(12716), new Item(12718) } };
+    companion object {
+        /**
+         * Represents the names of the puppys.
+         */
+        private val NAMES = arrayOf("labrador", "bulldog", "dalmatian", "greyhound", "terrier", "sheepdog")
 
-	@Override
-	public Plugin<Object> newInstance(Object arg) throws Throwable {
-		ComponentDefinition.put(668, this);
-		return this;
-	}
-
-	@Override
-	public boolean handle(Player player, Component component, int opcode, int button, int slot, int itemId) {
-		int index = button == 3 ? 1 : button == 8 ? 0 : button == 6 ? 1 : button == 4 ? 2 : button == 5 ? 3 : button == 6 ? 4 : 5;
-		player.getDialogueInterpreter().open(6893, NAMES[index], PUPPIES[index][RandomFunction.random(PUPPIES[index].length)]);
-		return true;
-	}
-
+        /**
+         * Represents the puppies.
+         */
+        private val PUPPIES = arrayOf(
+                arrayOf(Item(12516), Item(12708), Item(12710)), //Labrador
+                arrayOf(Item(12522), Item(12720), Item(12722)), //Bulldog
+                arrayOf(Item(12518), Item(12712), Item(12714)), //Dalmatian
+                arrayOf(Item(12514), Item(12704), Item(12706)), //Greyhound
+                arrayOf(Item(12512), Item(12700), Item(12702)), //Terrier
+                arrayOf(Item(12520), Item(12716), Item(12718))) //Sheepdog
+    }
 }
