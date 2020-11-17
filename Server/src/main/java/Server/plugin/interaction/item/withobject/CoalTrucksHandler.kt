587a8c2eb16c40a6ecc0a9b24b51bc24f737b4f8
Ceikry <ironceikry@gmail.com>
2020-11-16 20:33:02 -0600

    Fixed seers' village saving

--- Server/src/main/java/Server/plugin/interaction/item/withobject/CoalTrucksHandler.kt
+++ Server/src/main/java/Server/plugin/interaction/item/withobject/CoalTrucksHandler.kt
@@ -1,109 +1,97 @@
-package plugin.interaction.item.withobject;
+package plugin.interaction.item.withobject
 
-import core.cache.def.impl.ObjectDefinition;
-import core.game.interaction.NodeUsageEvent;
-import core.game.interaction.OptionHandler;
-import core.game.interaction.UseWithHandler;
-import core.game.node.Node;
-import core.game.node.entity.player.Player;
-import core.game.node.entity.player.link.diary.DiaryType;
-import core.game.node.item.Item;
-import core.plugin.InitializablePlugin;
-import core.plugin.Plugin;
-import core.plugin.PluginManager;
-
-import java.util.ArrayList;
-import java.util.List;
+import core.cache.def.impl.ObjectDefinition
+import core.game.interaction.NodeUsageEvent
+import core.game.interaction.OptionHandler
+import core.game.interaction.UseWithHandler
+import core.game.node.Node
+import core.game.node.entity.player.Player
+import core.game.node.entity.player.link.diary.DiaryType
+import core.game.node.item.Item
+import core.game.world.map.zone.ZoneBorders
+import core.plugin.InitializablePlugin
+import core.plugin.Plugin
+import core.plugin.PluginManager.definePlugin
+import core.tools.ItemNames
+import java.util.*
 
 /**
  * Handles coal truck interactions
  * @author ceik
  */
-
 @InitializablePlugin
-public class CoalTrucksHandler extends OptionHandler {
-    private static final Item COAL = new Item(453,1);
+class CoalTrucksHandler : OptionHandler() {
 
-    @Override
-    public Plugin<Object> newInstance(Object arg) throws Throwable {
-        PluginManager.definePlugin(new useCoalWithTruck());
-        ObjectDefinition def = ObjectDefinition.forId(2114);
-        def.getHandlers().put("option:remove-coal",this);
-        def.getHandlers().put("option:investigate",this);
-        return this;
+    val seersVillageTrucks = ZoneBorders(2690,3502,2699,3508)
+
+    override fun newInstance(arg: Any?): Plugin<Any>? {
+        definePlugin(useCoalWithTruck())
+        val def = ObjectDefinition.forId(2114)
+        def.handlers["option:remove-coal"] = this
+        def.handlers["option:investigate"] = this
+        return this
     }
 
-    @Override
-    public boolean handle(Player player, Node node, String option) {
-        int coalInTruck = player.getAttribute("coal-truck-inventory",0);
-        int freeSpace = player.getInventory().freeSlots();
-        List<Item> toAdd = new ArrayList<>();
-        switch(option){
-            case "remove-coal":
-                if(coalInTruck == 0) {
-                    player.getDialogueInterpreter().sendDialogue("The coal truck is empty.");
-                    return true;
+    override fun handle(player: Player, node: Node, option: String): Boolean {
+        var coalInTruck = player.getAttribute("coal-truck-inventory", 0)
+        var freeSpace = player.inventory.freeSlots()
+        val toAdd: MutableList<Item> = ArrayList()
+        when (option) {
+            "remove-coal" -> {
+                if (coalInTruck == 0) {
+                    player.dialogueInterpreter.sendDialogue("The coal truck is empty.")
+                    return true
                 }
-                while((coalInTruck > 0)){
-                    if(freeSpace > 0){
-                        toAdd.add(COAL);
-                        coalInTruck--;
-                        freeSpace--;
-                    } else {
-                        break;
-                    }
-                }
-                player.getInventory().addList(toAdd);
-                player.setAttribute("/save:coal-truck-inventory", coalInTruck);
-                if (coalInTruck == 0
-                        && !player.getAchievementDiaryManager().hasCompletedTask(DiaryType.SEERS_VILLAGE,1,2)
-                        && player.getViewport().getRegion().getId() == 10806 // region 10294 is at coal truck mine, region 10806 is in seers village
+                var toRemove = player.inventory.freeSlots()
+
+                if(toRemove > coalInTruck) toRemove = coalInTruck
+
+                player.inventory.add(Item(ItemNames.COAL,toRemove))
+
+                coalInTruck -= toRemove
+                player.setAttribute("/save:coal-truck-inventory", coalInTruck)
+
+
+                if (!player.achievementDiaryManager.hasCompletedTask(DiaryType.SEERS_VILLAGE, 1, 2)
+                        && seersVillageTrucks.insideBorder(player)
                         && player.getAttribute("diary:seers:coal-truck-full", false)) {
-                    player.removeAttribute("diary:seers:coal-truck-full");
-                    player.getAchievementDiaryManager().finishTask(player,DiaryType.SEERS_VILLAGE,1,2);
-                }
-                if (!player.getAchievementDiaryManager().hasCompletedTask(DiaryType.SEERS_VILLAGE,1,2)
-                        && player.getViewport().getRegion().getId() == 10294) { // region 10294 is at coal truck mine, region 10806 is in seers village
-                    player.setAttribute("/save:diary:seers:coal-truck-full", false);
-                }
-                break;
-            case "investigate":
-                player.getDialogueInterpreter().sendDialogue("There is currently " + coalInTruck + " coal in the truck.","The truck has space for " + (120 - coalInTruck) + " more coal.");
-                break;
-        }
-        return true;
-    }
-
-    public class useCoalWithTruck extends UseWithHandler{
-        public useCoalWithTruck(){
-            super(COAL.getId());
-        }
-        @Override
-        public Plugin<Object> newInstance(Object arg) throws Throwable {
-            addHandler(2114, OBJECT_TYPE,this);
-            return this;
-        }
-
-        @Override
-        public boolean handle(NodeUsageEvent event) {
-            Player player = event.getPlayer();
-            int coalInTruck = player.getAttribute("coal-truck-inventory",0);
-            while(player.getInventory().containsItem(COAL)){
-                if(coalInTruck < 120) {
-                    event.getPlayer().getInventory().remove(COAL);
-                    coalInTruck++;
-                }
-                if(coalInTruck >= 120) {
-                    event.getPlayer().getPacketDispatch().sendMessage("You have filled up the coal truck.");
-                    if (!player.getAchievementDiaryManager().getDiary(DiaryType.SEERS_VILLAGE).isComplete(1,2)
-                            && player.getViewport().getRegion().getId() == 10294) { // region 10294 is at coal truck mine, region 10806 is in seers village
-                        player.setAttribute("/save:diary:seers:coal-truck-full", true);
-                    }
-                    break;
+                    player.removeAttribute("diary:seers:coal-truck-full")
+                    player.achievementDiaryManager.finishTask(player, DiaryType.SEERS_VILLAGE, 1, 2)
                 }
             }
-            event.getPlayer().setAttribute("/save:coal-truck-inventory",coalInTruck);
-            return true;
+            "investigate" -> player.dialogueInterpreter.sendDialogue("There is currently $coalInTruck coal in the truck.", "The truck has space for " + (120 - coalInTruck) + " more coal.")
+        }
+        return true
+    }
+
+    inner class useCoalWithTruck : UseWithHandler(ItemNames.COAL) {
+        override fun newInstance(arg: Any?): Plugin<Any>? {
+            addHandler(2114, OBJECT_TYPE, this)
+            return this
+        }
+
+        override fun handle(event: NodeUsageEvent): Boolean {
+            val player = event.player
+            var coalInTruck = player.getAttribute("coal-truck-inventory", 0)
+
+            var coalInInventory = player.inventory.getAmount(ItemNames.COAL)
+
+            if(coalInInventory + coalInTruck >= 120){
+                coalInInventory = 120 - coalInTruck
+                event.player.packetDispatch.sendMessage("You have filled up the coal truck.")
+
+                //handle coal truck task for seer's village
+                if (!player.achievementDiaryManager.getDiary(DiaryType.SEERS_VILLAGE).isComplete(1, 2)
+                        && player.viewport.region.id == 10294) { // region 10294 is at coal truck mine, region 10806 is in seers village
+                    player.setAttribute("/save:diary:seers:coal-truck-full", true)
+                }
+            }
+
+            player.inventory.remove(Item(ItemNames.COAL,coalInInventory))
+            coalInTruck += coalInInventory
+
+            player.setAttribute("/save:coal-truck-inventory",coalInTruck)
+            return true
         }
     }
 }
