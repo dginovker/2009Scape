3cdf62bcb6b17b926d9035ecd0290e6d69701e00
Sir Kermit <BadHad@users.noreply.github.com>
2020-12-03 21:51:25 -0500

    Agility Shortcut Overhaul & More
    - Fixed and Implemented many shortcuts across the world!
    
    - Access to Swamp Summoning cave!!

--- Server/src/main/java/Server/plugin/skill/agility/shortcuts/SteppingStoneShortcut.kt
+++ Server/src/main/java/Server/plugin/skill/agility/shortcuts/SteppingStoneShortcut.kt
@@ -1,52 +1,85 @@
-package plugin.skill.agility.shortcuts;
+package plugin.skill.agility.shortcuts
 
-import plugin.skill.agility.AgilityShortcut;
-import core.game.node.entity.impl.ForceMovement;
-import core.game.node.entity.player.Player;
-import core.game.node.object.GameObject;
-import core.game.system.task.Pulse;
-import core.game.world.GameWorld;
-import core.game.world.update.flag.context.Animation;
-import core.plugin.InitializablePlugin;
+import core.cache.def.impl.ObjectDefinition
+import core.game.interaction.OptionHandler
+import core.game.node.Node
+import core.game.node.entity.impl.ForceMovement
+import core.game.node.entity.player.Player
+import core.game.system.task.Pulse
+import core.game.world.map.Location
+import core.game.world.update.flag.context.Animation
+import core.plugin.InitializablePlugin
+import core.plugin.Plugin
+import plugin.skill.Skills
 
 /**
  * Handles the stepping stone shortcut.
- * @author Vexia
+ * @author Ceikry
  */
 @InitializablePlugin
-public class SteppingStoneShortcut extends AgilityShortcut {
+class SteppingStoneShortcut : OptionHandler() {
+    private val stones = HashMap<Location,SteppingStoneInstance>()
+    internal class SteppingStoneInstance(val pointA: Location, val pointB: Location, val option: String, val levelReq: Int)
 
-	/**
-	 * Represents the animation to use.
-	 */
-	private static final Animation ANIMATION = new Animation(741);
+    override fun handle(player: Player?, node: Node?, option: String?): Boolean {
+        player ?: return false
+        val stone = stones[player.location]
+        stone ?: return false
 
-	/**
-	 * Constructs a new {@Code SteppingStoneShortcut} {@Code
-	 * Object}
-	 */
-	public SteppingStoneShortcut() {
-		super(new int[] { 9315 }, 1, 0.0, "jump-onto");
-	}
+        if(player.skills.getLevel(Skills.AGILITY) < stone.levelReq){
+            player.sendMessage("You need an agility level of ${stone.levelReq} for this shortcut.")
+            return true
+        }
 
-	@Override
-	public void run(final Player player, GameObject object, String option, boolean failed) {
-		final int offset = player.getLocation().getX() == 3149 ? 1 : -1;
-		player.lock();
-		GameWorld.getPulser().submit(new Pulse(2, player) {
-			int counter = 1;
+        val finalDest = when(player.location){
+            stone.pointA -> stone.pointB
+            stone.pointB -> stone.pointA
+            else -> player.location
+        }
+        val offset = getOffset(player,finalDest)
+        player.debug("Offset: ${offset.first},${offset.second}")
+        player.pulseManager.run(object : Pulse(2){
+            override fun pulse(): Boolean {
+                val there = player.location == finalDest
+                if(!there){
+                    ForceMovement.run(player,player.location,player.location.transform(offset.first,offset.second,0), ANIMATION,10)
+                }
+                return there
+            }
+        })
+        return true
+    }
 
-			@Override
-			public boolean pulse() {
-				if (counter == 6) {
-					player.unlock();
-					return true;
-				}
-				ForceMovement.run(player, player.getLocation(), player.getLocation().transform(offset, 0, 0), ANIMATION, 10);
-				counter++;
-				return false;
-			}
-		});
-	}
+    fun getOffset(player: Player, location: Location): Pair<Int,Int>{
+        var diffX = location.x - player.location.x
+        var diffY = location.y - player.location.y
+        if(diffX > 1) diffX = 1
+        if(diffX < -1) diffX = -1
+        if(diffY > 1) diffY = 1
+        if(diffY < -1) diffY = -1
+        return Pair(diffX,diffY)
+    }
 
+    fun configure(objects: IntArray, pointA: Location, pointB: Location, option: String, levelReq: Int){
+        val instance = SteppingStoneInstance(pointA,pointB, option, levelReq)
+        objects.forEach {
+            ObjectDefinition.forId(it).handlers["option:$option"] = this
+        }
+        stones.put(pointA,instance)
+        stones.put(pointB, instance)
+    }
+
+    override fun newInstance(arg: Any?): Plugin<Any> {
+        configure(intArrayOf(2335,2333),Location.create(2925,2947,0),Location.create(2925,2951,0),"cross",30)
+        configure(intArrayOf(9315),Location.create(3149, 3363, 0),Location.create(3154, 3363, 0),"jump-onto",31)
+
+        return this
+    }
+
+    companion object {
+        /**
+         * Represents the animation to use.
+         */
+        private val ANIMATION = Animation(741)
+    }
 }
